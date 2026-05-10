cb70d069a28f3af59109d0ac9a8cfbbc1bd9946a
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2026-05-10 02:36:46 +0100

    Switch to better and more compatible implementation

--- src/main/java/net/pneumono/riptideshieldfix/mixin/ItemInHandRendererMixin.java
+++ src/main/java/net/pneumono/riptideshieldfix/mixin/ItemInHandRendererMixin.java
@@ -5,8 +5,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.InteractionHand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -19,7 +18,11 @@ public class ItemInHandRendererMixin {
                     target = "Lnet/minecraft/client/player/AbstractClientPlayer;isAutoSpinAttack()Z"
             )
     )
-    private boolean preventAutoSpinAttackAnimation(AbstractClientPlayer instance, Operation<Boolean> original, @Local(argsOnly = true) ItemStack stack) {
-        return original.call(instance) && stack.getEnchantments().keySet().stream().anyMatch(enchantment -> enchantment.is(Enchantments.RIPTIDE));
+    private boolean preventAutoSpinAttackAnimation(
+            AbstractClientPlayer player,
+            Operation<Boolean> original,
+            @Local(argsOnly = true) InteractionHand hand
+    ) {
+        return original.call(player) && player.getUsedItemHand() == hand;
     }
 }
